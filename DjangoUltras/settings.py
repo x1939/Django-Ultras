8efc713b789d820d9356e1d182cecba37ef2795a
1939 <buterbrodminecraft@gmail.com>
2024-01-02 22:07:45 +0400

    -

--- DjangoUltras/settings.py
+++ DjangoUltras/settings.py
@@ -138,14 +138,17 @@ USE_I18N = True
 USE_TZ = True
 
 
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / 'static'
 ]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # 'data' is my media folder
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- DjangoUltras/urls.py
+++ DjangoUltras/urls.py
@@ -24,10 +24,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('i18n/', include('django.conf.urls.i18n')),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-urlpatterns += i18n_patterns(
+urlpatterns += i18n_patterns (
     path('', include('core.urls')),
     path('user/', include('user.urls')),
-    path('ckeditor/', include('ckeditor_uploader.urls')),
 )
