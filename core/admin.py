7b4ae690ecb25c56001410a602d1e8214b9754b8
1939 <buterbrodminecraft@gmail.com>
2024-01-02 23:04:57 +0400

    -

--- core/admin.py
+++ core/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from modeltranslation.admin import TranslationAdmin
 from .models import *
 
 admin.site.register(Contact)
@@ -8,13 +9,13 @@ admin.site.register(Logo)
 admin.site.register(Comment)
 
 @admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
+class CategoryAdmin(TranslationAdmin):
     search_fields = ('title',)
 
 @admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
+class ProductAdmin(TranslationAdmin):
     search_fields = ('title',)
 
 @admin.register(Blog)
-class BlogAdmin(admin.ModelAdmin):
+class BlogAdmin(TranslationAdmin):
     search_fields = ('title',)
